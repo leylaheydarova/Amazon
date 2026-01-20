226ae9038bbf14ed5e91568cbe6a2ae4d9e01308
leylaheydarova <leylaheydarova.dev@gmail.com>
2026-01-20 21:30:44 +0400

    models

---
+++ Amazon/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace Amazon.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Amazon/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Amazon.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double DiscountedPrice { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
